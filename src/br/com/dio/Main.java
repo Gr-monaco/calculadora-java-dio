1d8f6a1830a69100e7bba4ca78f68fd8ea7d0fff
Gabriel Monaco <gr-monaco@hotmail.com>
2022-01-19 21:51:25 -0300

    Adicionar calculadora e usar os métodos

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -3,6 +3,16 @@ package br.com.dio;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Calculadora cal = new Calculadora();
+
+        float a = cal.somaFloat(1f, 2f);
+        float b = cal.subtracaoFloat(1f, 2f);
+        float c = cal.multiplicacaoFloat(1f, 3f);
+        float d = cal.divisaoFloat(2f, 2f);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
