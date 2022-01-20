82414540eb09b09a6c3e5b885da09fc522c88af0
Gabriel Monaco <gr-monaco@hotmail.com>
2022-01-19 21:44:25 -0300

    Adicionar funções aritmeticas básicas para float

--- src/br/com/dio/Calculadora.java
+++ src/br/com/dio/Calculadora.java
@@ -1,4 +1,20 @@
 package br.com.dio;
 
 public class Calculadora {
+
+    public float somaFloat(float A, float B){
+        return A+B;
+    }
+
+    public float subtracaoFloat(float A, float B){
+        return A-B;
+    }
+
+    public float multiplicacaoFloat(float A, float B){
+        return A*B;
+    }
+
+    public float divisaoFloat(float A,float B){
+        return A/B;
+    }
 }
